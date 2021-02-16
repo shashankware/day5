b7c4de0066643a9b9e929045beb41d168d9165a8
shashank <shashank.w1998@gmail.com>
2021-02-16 20:41:20 +0530

    Marjune Added

--- leap.sh
+++ leap.sh
@@ -20,3 +20,22 @@ then
 else
    echo "$yy is NOT leap year"
 fi
+
+2.	#!/bin/bash -x
+
+read -p " Enter Date:-" date
+read -p " Enter Month:-" Month
+
+if (( ($Month <= 6 & $date <= 20) ))
+then
+        echo $Month $date "True";
+
+elif (( ($Month >= 3 & $Month < 6) & ($date<31)  ))
+then
+        echo $date $Month "True";
+
+else
+
+        echo "False";
+fi
+
